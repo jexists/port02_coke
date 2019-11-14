34ee9f7c4975081eaf8b69b370868cffe6ddc954
jexists <jexists486@gmail.com>
2019-11-14 18:11:05 +0900

    code on 191114

---
+++ code/js/src/jquery/main_slide.js
@@ -0,0 +1,34 @@
+//main_slide.js
+
+(function($){
+const imgUrl = '../img/main_slide/';
+const slide = [
+{class: 'ad_01', h5:'코카콜라 들고 뉴욕여행', link:'#',bg:'new_york_bg.png', text:'코카콜라 들고 뉴욕여행',main_img:'new_york_coke.png'},
+{class: 'ad_02', h5:'코카콜라 광고2', link:'#',bg:'new_york_bg.png', text:'광고',main_img:'/png'},
+{class: 'ad_03', h5:'코카콜라 광고3', link:'#',bg:'new_york_bg.png', text:'광고',main_img:'/png'}
+]
+const adBox = $('#adBox');
+const nextBtn = adBox.find('.next_btn').children('button');
+const prevBtn = adBox.find('.prev_btn').children('button');
+const slWrap = adBox.find('#slide_wrap')
+
+slWrap.css({width:/* slide.length * */ 100 + '%'})
+
+
+for(let i = 0; i < slide.length ;i++){
+	slWrap.append('<div></div>');
+	const artBox = slWrap.children('div').eq(i)
+	artBox.addClass(slide[i].class);
+	artBox.append('<h5 class="hd"></h5>');
+	artBox.children('h5').text(slide[i].h5);
+	artBox.css({backgroundImage:'url('+imgUrl+slide[i].bg+')'});
+	artBox.append('<div class="Mimg_0'+(i+1)+'"></div>');
+};
+slWrap.children('div').css({width: 100/*/slide.length*/ + '%'})
+/*링크 문자 메인사진*/
+slWrap.children('div').eq(0).children('div').css({backgroundImage:'url('+imgUrl+slide[0].main_img+')'})
+slWrap.children('div').eq(0).append('<p class="td"><span>new</span><span>york</span></p>')
+slWrap.children('div').eq(0).append('<p class="tt"></p>');
+slWrap.children('div').eq(0).find('.tt').text(slide[0].text)
+slWrap.children('div').eq(0).siblings().hide();
+})(jQuery);
